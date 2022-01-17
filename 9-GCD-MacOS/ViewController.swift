52f592fda0a9349d58323889fcc29fba86afa52e
Baris Karalar <karalarbaris@gmail.com>
2022-01-17 14:25:54 +0100

    Initial Commit

---
+++ 9-GCD-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  9-GCD-MacOS
+//
+//  Created by Baris Karalar on 17.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
