96454d45a433b92a28bb1a633adbfbfdea0d49e7
Baris Karalar <karalarbaris@gmail.com>
2022-01-17 15:48:18 +0100

    Running arbitrary code on different threads

--- 9-GCD-MacOS/ViewController.swift
+++ 9-GCD-MacOS/ViewController.swift
@@ -8,18 +8,18 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        runBackgorundCode1()
-       
+        runSynchronousCode()
+        
         
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
     
@@ -27,13 +27,49 @@ class ViewController: NSViewController {
         print("Printing message: \(message)")
     }
     
-
-    func runBackgorundCode1() {
+    
+    func runBackgroundCode1() {
         performSelector(inBackground: #selector(log), with: "Hello world 1")
-
+        
         performSelector(onMainThread: #selector(log), with: "Hello world 2", waitUntilDone: false)
         
         log(message: "Hello world 3")
+    }
+    
+    func runBackgroundCode2() {
+        DispatchQueue.global().async { [unowned self] in
+            self.log(message: "On background thread")
+            DispatchQueue.main.async {
+                self.log(message: "On main thread")
+            }
+        }
+    }
+    
+    func runBackgroundCode3() {
+        DispatchQueue.global().async {
+            guard let url = URL(string: "https://www.apple.com") else { return }
+            guard let str = try? String(contentsOf: url) else { return }
+            print(str)
+        }
+    }
+    
+    func runBackgroundCode4() {
+        DispatchQueue.global(qos: .userInteractive).async { [unowned self] in
+            self.log(message: "This is high priority")
+        }
+    }
+    
+    func runSynchronousCode() {
+       // asynchronous!
+       DispatchQueue.global().async {
+          print("Background thread 1")
+       }
+       print("Main thread 1")
+       // synchronous!
+       DispatchQueue.global().sync {
+          print("Background thread 2")
+    }
+       print("Main thread 2")
     }
 }
 
